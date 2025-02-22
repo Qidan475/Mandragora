aeb906c9ee8921cf5fe305b2f887e22cabca830e
Qidan475 <gamerok2016gm.c@gmail.com>
2025-02-22 17:16:01 +0700

    Add files via upload

---
+++ Mandragora/EntryPoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using HarmonyLib;
+
+namespace Mandragora
+{
+    public class EntryPoint : Plugin<PluginConfig>
+    {
+        public override string Name => "Mandragora";
+        public override string Author => "dark7eamplar.69420";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(9, 5, 0);
+
+        public static CachedLayerMask WallLayerMask { get; private set; }
+
+        public static EntryPoint Instance { get; private set; }
+
+        private Harmony _harmony;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            _harmony = new Harmony($"{Author}.{Name}");
+            GlobalPatchProcessor.PatchAll(_harmony, out var failed);
+            if (failed > 0)
+                Log.Error("failed patching");
+
+            WallLayerMask = new CachedLayerMask(Config.LinecastWallLayerMasks);
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Instance = null;
+            GlobalPatchProcessor.UnpatchAll(_harmony.Id);
+
+            base.OnDisabled();
+        }
+    }
+
+    public class PluginConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+
+        [Description("Default base-game value is 0.15. You can tweak it by setting the value lower, so it will be stricter. Min value - 0")]
+        public float ForecastOverride { get; set; } = 0.15f;
+        public float MinVelocityRequiredToCheckForecast { get; set; } = 0.3f;
+        [Description("The main thing that protects from this type of exploit. It should not be that expensive, but you can try to disable it, if it is, and set forecast_override to zero")]
+        public bool LinecastCheck { get; set; } = true;
+        public string[] LinecastWallLayerMasks { get; set; } = new string[]
+        {
+            "Default",
+            "Door",
+            "Glass"
+        };
+    }
+}
---
+++ Mandragora/Patches/FpcBacktrackPatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Features.Toys;
+using HarmonyLib;
+using PlayerRoles.FirstPersonControl;
+using PlayerStatsSystem;
+using UnityEngine;
+
+namespace Mandragora.Patches
+{
+    [HarmonyPatch(typeof(FpcBacktracker))]
+    internal class FpcBacktrackPatch
+    {
+        [HarmonyPrefix()]
+        [HarmonyPatch(MethodType.Constructor, typeof(ReferenceHub), typeof(Vector3), typeof(Quaternion), typeof(float), typeof(float), typeof(bool), typeof(bool))]
+        static bool BacktrackerConstructorPrefix(FpcBacktracker __instance, ReferenceHub hub, Vector3 claimedPos, Quaternion claimedRot, float backtrack, float forecast, bool ignoreTp, bool restoreUponDeath)
+        {
+            forecast = EntryPoint.Instance.Config.ForecastOverride;
+
+            if (hub.roleManager.CurrentRole is IFpcRole fpcRole)
+            {
+                FirstPersonMovementModule fpcModule = fpcRole.FpcModule;
+                var prevPos = fpcModule.Position;
+                var velocity = fpcModule.Motor.Velocity;
+
+                __instance._moved = true;
+                AccessTools.Field(typeof(FpcBacktracker), nameof(FpcBacktracker._movedHub)).SetValue(__instance, hub);
+                AccessTools.Field(typeof(FpcBacktracker), nameof(FpcBacktracker._prevPos)).SetValue(__instance, prevPos);
+                AccessTools.Field(typeof(FpcBacktracker), nameof(FpcBacktracker._prevRot)).SetValue(__instance, hub.PlayerCameraReference.rotation);
+                Bounds bounds = ((backtrack <= 0f) ? new Bounds(fpcModule.Position, Vector3.zero) : fpcModule.Tracer.GenerateBounds(backtrack, ignoreTp));
+                if (forecast > 0f && velocity.magnitude > EntryPoint.Instance.Config.MinVelocityRequiredToCheckForecast)
+                {
+                    Vector3 predictedPosition = prevPos + velocity * forecast;
+                    Vector3 allowedPosition = predictedPosition;
+                    if (EntryPoint.Instance.Config.LinecastCheck && Physics.Linecast(prevPos, predictedPosition, out var hit, EntryPoint.WallLayerMask))
+                    {
+                        var directionToPlayer = (prevPos - hit.point).normalized;
+                        Vector3 posCloserToPlayer = hit.point + (directionToPlayer * 0.05f);
+                        allowedPosition = posCloserToPlayer;
+                    }
+                    bounds.Encapsulate(allowedPosition);
+                }
+                AccessTools.Field(typeof(FpcBacktracker), nameof(FpcBacktracker._newPos)).SetValue(__instance, bounds.ClosestPoint(claimedPos));
+                fpcModule.Position = __instance._newPos;
+                hub.PlayerCameraReference.rotation = claimedRot;
+            }
+            else
+            {
+                __instance._moved = false;
+            }
+            if (restoreUponDeath)
+            {
+                PlayerStats.OnAnyPlayerDied += __instance.OnDied;
+                __instance._restoreUponDeath = true;
+                return false;
+            }
+            __instance._restoreUponDeath = false;
+
+            return false;
+        }
+    }
+}
