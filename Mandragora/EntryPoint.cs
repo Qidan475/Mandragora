b0dcb7f72ce16d79222bf02afa998fcd15238921
Qidan475 <48491875+Qidan475@users.noreply.github.com>
2025-03-16 00:49:36 +0700

    bump version

--- Mandragora/EntryPoint.cs
+++ Mandragora/EntryPoint.cs
@@ -18,7 +18,7 @@ namespace Mandragora
     {
         public override string Name => "Mandragora";
         public override string Author => "dark7eamplar.69420";
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 2, 0);
         public override Version RequiredExiledVersion { get; } = new Version(9, 5, 0);
 
         public CachedLayerMask WallLayerMask { get; private set; }
